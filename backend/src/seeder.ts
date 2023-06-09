76c82d7112da79c2a18016d8a33726518fd1de4d
Nachiket <nmore@pdx.edu>
2023-06-09 15:31:48 -0700

    added seeder

---
+++ backend/src/seeder.ts
@@ -0,0 +1,47 @@
+export const seedQuestions = [
+    {
+        "question": "Which tool is commonly used for version control in software development?",
+        "options": {
+            "1": "GitHub",
+            "2": "Docker",
+            "3": "Jenkins"
+        },
+        "answer": 1
+    },
+    {
+        "question": "Which programming language is often used for building server-side applications with Node.js?",
+        "options": {
+            "1": "JavaScript",
+            "2": "Python",
+            "3": "Ruby"
+        },
+        "answer": 1
+    },
+    {
+        "question": "What is the purpose of unit testing in software development?",
+        "options": {
+            "1": "To test the entire application as a whole",
+            "2": "To ensure individual components or functions work correctly",
+            "3": "To perform performance testing"
+        },
+        "answer": 2
+    },
+    {
+        "question": "Which technology is commonly used for containerization and deploying applications?",
+        "options": {
+            "1": "Docker",
+            "2": "Express",
+            "3": "React"
+        },
+        "answer": 1
+    },
+    {
+        "question": "What is the purpose of continuous integration in software development?",
+        "options": {
+            "1": "To automatically deploy applications to production servers",
+            "2": "To ensure code changes are merged and tested frequently",
+            "3": "To monitor application performance in real-time"
+        },
+        "answer": 2
+    }
+  ]
